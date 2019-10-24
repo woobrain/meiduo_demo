b5479ce81202bf5ad96d5e8b6877f6f424ca2145
woobrain12 <15893775982@163.com>
2019-10-24 21:47:02 +0800

    初级商品详情页混合功能实现

--- meiduo1/apps/goods/urls.py
+++ meiduo1/apps/goods/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$',views.ListView.as_view(),name='list'),
-    url(r'^hot/(?P<category_id>\d+)/$',views.HotView.as_view(),name='hot')
+    url(r'^hot/(?P<category_id>\d+)/$',views.HotView.as_view(),name='hot'),
+    url(r'^detail/(?P<sku_id>\d+)/$',views.DetailView.as_view(),name='detail'),
 
 ]
--- meiduo1/apps/goods/views.py
+++ meiduo1/apps/goods/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseBadRequest
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -6,6 +7,7 @@ from django.core.paginator import Paginator
 from django.views import View
 
 from apps.goods.models import GoodsCategory, SKU
+from apps.goods.utils import get_breadcrumb
 from apps.user1.utils import get_categories
 
 
@@ -17,22 +19,7 @@ class ListView(View):
             return render(request,'404.html')
         sort = request.GET.get('sort','default')
         categories = get_categories()
-        breadcrumb={
-            "cat1":'',
-            "cat2":'',
-            "cat3":'',
-
-        }
-        # a = category.subs
-        if category.parent is None:
-            breadcrumb['cat1']=category
-        elif category.subs.count() == 0:
-            breadcrumb['cat3']=category
-            breadcrumb['cat2']=category.parent
-            breadcrumb['cat1']=category.parent.parent
-        else:
-            breadcrumb['cat2']=category
-            breadcrumb['cat1']=category.parent
+        breadcrumb = get_breadcrumb(category)
 
         if sort == 'price':
             sort_field = 'price'
@@ -85,3 +72,73 @@ class HotView(View):
         a=hot_list
 
         return JsonResponse({"code":0,"errmsg":"ok","hot_skus":hot_list})
+
+
+from django import http
+from django.shortcuts import render
+from django.views import View
+from apps.user1.utils import get_categories
+from apps.goods.models import SKU, GoodsCategory
+
+from utils.response_code import RETCODE
+
+
+class DetailView(View):
+
+    def get(self,request,sku_id):
+
+        # 获取当前sku的信息
+        try:
+            sku = SKU.objects.get(id=sku_id)
+        except SKU.DoesNotExist:
+            return render(request, '404.html')
+
+        # 查询商品频道分类
+        categories = get_categories()
+        # 查询面包屑导航
+        breadcrumb = get_breadcrumb(sku.category)
+
+        # 构建当前商品的规格键
+        sku_specs = sku.specs.order_by('spec_id')
+        sku_key = []
+        for spec in sku_specs:
+            sku_key.append(spec.option.id)
+        # 获取当前商品的所有SKU
+        skus = sku.spu.sku_set.all()
+        # 构建不同规格参数（选项）的sku字典
+        spec_sku_map = {}
+        for s in skus:
+            # 获取sku的规格参数
+            s_specs = s.specs.order_by('spec_id')
+            # 用于形成规格参数-sku字典的键
+            key = []
+            for spec in s_specs:
+                key.append(spec.option.id)
+            # 向规格参数-sku字典添加记录
+            spec_sku_map[tuple(key)] = s.id
+        # 获取当前商品的规格信息
+        goods_specs = sku.spu.specs.order_by('id')
+        # 若当前sku的规格信息不完整，则不再继续
+        if len(sku_key) < len(goods_specs):
+            return
+        for index, spec in enumerate(goods_specs):
+            # 复制当前sku的规格键
+            key = sku_key[:]
+            # 该规格的选项
+            spec_options = spec.options.all()
+            for option in spec_options:
+                # 在规格参数sku字典中查找符合当前规格的sku
+                key[index] = option.id
+                option.sku_id = spec_sku_map.get(tuple(key))
+            spec.spec_options = spec_options
+
+        # 渲染页面
+        context = {
+            'categories': categories,
+            'breadcrumb': breadcrumb,
+            'sku': sku,
+            'specs': goods_specs,
+        }
+
+        return render(request,'detail.html',context)
+
